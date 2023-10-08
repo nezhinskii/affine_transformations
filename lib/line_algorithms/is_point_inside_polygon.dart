40391f247976e1ede603a7a2a78f0e9b116c2869
Ksum7 <xamchik02@gmail.com>
2023-10-08 17:13:21 +0300

    add isPointInsidePolygon

---
+++ lib/line_algorithms/is_point_inside_polygon.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+
+import 'package:affine_transformations/primitives.dart';
+import 'package:affine_transformations/line_algorithms/point_relative_to_line.dart';
+
+bool isPointInsidePolygon(Point point, Polygon polygon) {
+  bool res = false;
+  for (int i = 0; i < polygon.vertices.length; i++) {
+    Offset minPoint = polygon.vertices[i];
+    Offset maxPoint = polygon.vertices[(i + 1) % polygon.vertices.length];
+    if (minPoint.dy > maxPoint.dy) (minPoint, maxPoint) = (maxPoint, minPoint);
+
+    if (maxPoint.dy <= point.position.dy || minPoint.dy > point.position.dy)
+      continue;
+
+    switch (pointRelativeToLineAsOffsets(point.position, minPoint, maxPoint)) {
+      case RelativePointPosition.within:
+        return true;
+      case RelativePointPosition.left:
+        res != res;
+        break;
+      default:
+    }
+  }
+
+  return res;
+}
--- lib/line_algorithms/point_relative_to_line.dart
+++ lib/line_algorithms/point_relative_to_line.dart
@@ -2,16 +2,33 @@ import 'dart:ui';
 
 import 'package:affine_transformations/primitives.dart';
 
-enum RelativePointPosition{
-  left, right,
+enum RelativePointPosition {
+  left,
+  right,
+  within,
 }
 
-RelativePointPosition pointRelativeToLine(Point point, Line line){
+RelativePointPosition pointRelativeToLine(Point point, Line line) {
   final b = line.endPosition - line.startPosition;
   final a = point.position - line.startPosition;
   final sa = a.dx * b.dy - b.dx * a.dy;
-  if (sa > 0) {
-    return RelativePointPosition.left;
-  }
-  return RelativePointPosition.right;
+
+  return sa == 0
+      ? RelativePointPosition.within
+      : sa > 0
+          ? RelativePointPosition.left
+          : RelativePointPosition.right;
+}
+
+RelativePointPosition pointRelativeToLineAsOffsets(
+    Offset point, Offset linePoint1, Offset linePoint2) {
+  final b = linePoint2 - linePoint1;
+  final a = point - linePoint1;
+  final sa = a.dx * b.dy - b.dx * a.dy;
+
+  return sa == 0
+      ? RelativePointPosition.within
+      : sa > 0
+          ? RelativePointPosition.left
+          : RelativePointPosition.right;
 }
