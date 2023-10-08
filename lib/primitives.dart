e6e6cfc3f78b43f4ae5d3671c33727e23f73eed6
Ksum7 <xamchik02@gmail.com>
2023-10-08 16:53:51 +0300

    some renaming

--- lib/primitives.dart
+++ lib/primitives.dart
@@ -1,21 +1,21 @@
 import 'dart:ui';
 
-sealed class Primitive{
+sealed class Primitive {
   bool isSelected = false;
 }
 
-class Point extends Primitive{
+class Point extends Primitive {
   Offset position;
   Point(this.position);
 }
 
-class Line extends Primitive{
+class Line extends Primitive {
   Offset startPosition;
   Offset endPosition;
   Line(this.startPosition, this.endPosition);
 }
 
-class Polygon extends Primitive{
-  List<Offset> vertexes;
-  Polygon(this.vertexes);
+class Polygon extends Primitive {
+  List<Offset> vertices;
+  Polygon(this.vertices);
 }
--- lib/primitives_painter.dart
+++ lib/primitives_painter.dart
@@ -3,41 +3,43 @@ import 'dart:ui';
 import 'package:affine_transformations/primitives.dart';
 import 'package:flutter/material.dart';
 
-class PrimitivesPainter extends CustomPainter{
+class PrimitivesPainter extends CustomPainter {
   final List<Primitive> primitives;
   final Paint style;
   final List<Offset>? newPoints;
 
-  const PrimitivesPainter({
-    required this.primitives,
-    required this.style,
-    this.newPoints
-  });
+  const PrimitivesPainter(
+      {required this.primitives, required this.style, this.newPoints});
 
   @override
   void paint(Canvas canvas, Size size) {
-    for (var primitive in primitives){
-      var paint = Paint()..strokeWidth = style.strokeWidth..color = style.color;
-      if (primitive.isSelected){
+    for (var primitive in primitives) {
+      var paint = Paint()
+        ..strokeWidth = style.strokeWidth
+        ..color = style.color;
+      if (primitive.isSelected) {
         paint.color = Colors.red;
       }
-      switch (primitive){
+      switch (primitive) {
         case Point():
           canvas.drawPoints(PointMode.points, [primitive.position], paint);
         case Line():
-          canvas.drawLine(primitive.startPosition, primitive.endPosition, paint);
+          canvas.drawLine(
+              primitive.startPosition, primitive.endPosition, paint);
         case Polygon():
-          for(int i = 1; i < primitive.vertexes.length; ++i){
-            canvas.drawLine(primitive.vertexes[i - 1], primitive.vertexes[i], paint);
+          for (int i = 1; i < primitive.vertices.length; ++i) {
+            canvas.drawLine(
+                primitive.vertices[i - 1], primitive.vertices[i], paint);
           }
-          canvas.drawLine(primitive.vertexes.first, primitive.vertexes.last, paint);
+          canvas.drawLine(
+              primitive.vertices.first, primitive.vertices.last, paint);
       }
     }
     if (newPoints != null) {
-      if (newPoints!.length == 1){
+      if (newPoints!.length == 1) {
         canvas.drawPoints(PointMode.points, [newPoints!.first], style);
       }
-      for(int i = 1; i < newPoints!.length; ++i){
+      for (int i = 1; i < newPoints!.length; ++i) {
         canvas.drawLine(newPoints![i - 1], newPoints![i], style);
       }
     }
@@ -47,5 +49,4 @@ class PrimitivesPainter extends CustomPainter{
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
   }
-
 }
